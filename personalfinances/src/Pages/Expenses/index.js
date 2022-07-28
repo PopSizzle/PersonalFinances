5e943d06d11437200dd929821a5e62c1ae988706
Sam Poppe <poppe.samuel@gmail.com>
2022-07-28 16:12:03 -0700

    fixed bugs while submitting expenses
    fixed bugs with capitalization and finding the id of the current expense

--- personalfinances/src/Pages/Expenses/index.js
+++ personalfinances/src/Pages/Expenses/index.js
@@ -40,37 +40,58 @@ const Expenses = () => {
   )
 
   let title1 = 'Expense Tracker';
-  let cols1 = ['Date', 'Description', 'Cateogry', 'Cost'];
+  let cols1 = ['Date', 'Description', 'Category', 'Cost'];
 
   let title2 = 'Total Expenses';
   let cols2 = ['Category', 'Total'];
 
   let form1Inputs = ['date', 'description', 'category', 'cost']
 
-  const handleExpenseSubmit = (e) => {
-    e.preventDefault();
-
+  const getNextId = (object) =>{
+    
     let id;
-    let keys = Object.keys(expenseData);
+    let keys = Object.keys(object);
   
     // Find the next available id
-    for(let i=0; i<=keys.length+1; i++){
-      console.log(parseInt(keys[i]));
-      if(parseInt(keys[i]) !== i){
-        id=i;
-        break;
+    for(let i=0; i<keys.length+1; i++){
+      if(!keys[i] || parseInt(keys[i]) !== i+1){
+      id=i+1;
+      break;
       }
     }
 
-    setCurrExpense({id: id, ...currExpense})
+    console.log(id)
+
+    return id;
+  }
+
+  const handleExpenseSubmit = (e) => {
+    e.preventDefault();
+
+    let id = getNextId(expenseData);
+    let expense = currExpense
+
+    setCurrExpense({id: id, ...expense});
+
+    console.log(currExpense);
+
+    let totals = expenseTotals;
+    let cat = currExpense.category;
+    if(!totals[cat]){
+      totals[cat] = {
+        id: getNextId(totals),
+        category: cat,
+        total: 0
+      }
+    }
+
+    totals[currExpense.category].total = parseFloat(totals[currExpense.category].total) + parseFloat(currExpense.cost);
+    setExpenseTotals(totals);
 
     let expenses = expenseData;
     expenses[id] = currExpense;
+    expenses[id].category = cat[0].toUpperCase() + cat.slice(1);
     setExpenseData(expenses);
-
-    let totals = expenseTotals;
-    totals[currExpense.category].total = parseFloat(totals[currExpense.category].total) + parseFloat(currExpense.cost);
-    setExpenseTotals(totals);
     
   }
 
@@ -78,7 +99,7 @@ const Expenses = () => {
     let key = e.target.id;
 
     let expense = currExpense;
-    expense[key] = e.target.value
+    expense[key] = e.target.value.toLowerCase();
     setCurrExpense(expense);
   }
 
