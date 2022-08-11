5ae78d41b789589da003d68c11c96164ef8c5f46
Sam Poppe <poppe.samuel@gmail.com>
2022-08-10 21:13:17 -0700

    Removed hooks folder
    Removed hooks folder to simplify application

--- personalfinances/src/Pages/Exercise/index.js
+++ personalfinances/src/Pages/Exercise/index.js
@@ -1,7 +1,6 @@
 import { React, useEffect, useState } from 'react';
 import Table from '../../Components/Table';
 import Form from '../../Components/Form';
-import handleSubmit from '../../Hooks/handleSubmit';
 
 const Exercise = () => {
 
@@ -14,7 +13,6 @@ const Exercise = () => {
 
   let title1 = 'Exercise Log';
   let cols1 = ['Date', 'Type', 'Description'];
-  let data1 = [];
 
   let form1Inputs = ['date','type','description'];
   let form1Title = 'Add Exercise';
@@ -76,7 +74,7 @@ const Exercise = () => {
 
   return (
     <div>
-      <Form inputs={form1Inputs} title={form1Title} handleChange={e=>handleExerciseChange(e)} handleSubmit={e=>handleSubmit(e, form1Inputs, currExercise, exerciseData, setExerciseData, 'exercises')} clear={clearExerciseForm} />
+      <Form inputs={form1Inputs} title={form1Title} handleChange={e=>handleExerciseChange(e)} handleSubmit={e=>handleExerciseSubmit(e)} clear={clearExerciseForm} />
       <Table title={title1} cols={cols1} data={exerciseData} edit={true} deleteFunction={e=> deleteExercise(e)}/>
     </div>
   )
